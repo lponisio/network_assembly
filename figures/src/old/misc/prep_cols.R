a91ae8146822231eb613bc3045638605632bf8e9
lponisio <lponisio@gmail.com>
2017-03-02 12:43:00 -0800

    deleted all the old figure code

--- figures/src/old/misc/prep_cols.R
+++
@@ -1,9 +0,0 @@
-
-
-prep.cols <- function(dats){
-	cInt <- classIntervals(dats, style="fisher")
-	cPal <- tim.colors(24)
-	cols  <- findColours(cInt,cPal)
-	cols   <- ifelse(cols == "#00008F", "white", cols ) 
-	return(cols)
-}
