69e3b93869baf97b526137f981fc22bfa51d7047
lponisio <lponisio@gmail.com>
2017-05-11 15:01:49 -0700

    added packages to install

--- simulation/src/all/initialize.R
+++ simulation/src/all/initialize.R
@@ -1,3 +1,14 @@
+## ## packages to install
+## install.packages("TreeSim")
+## install.packages("ape")
+## install.packages("geiger")
+## install.packages("mvtnorm")
+## install.packages("bipartite")
+## install.packages("igraph")
+## install.packages("parallel")
+## install.packages("vegan")
+## install.packages("RColorBrewer")
+
 library(TreeSim)
 library(ape)
 library(geiger)
@@ -13,54 +24,59 @@ source('simulation/src/all/LoadFix.R')
 source('simulation/src/all/SimTreeTrait.R')
 
 if(tree == 'coal'){
-  source('simulation/src/coal/all/SimPhyloCoal.R')
-  source('simulation/src/coal/CoalMaster.R')
-  sim.dir <- 'simulation/saved/coal'
-  fig.dir <- 'figures/coal'
-  base.prms <- function() {
-    prms.f(list(ntree= 1, ## do not change
-                nrule= 2,
-                nmat= 1,
-                nulls= 99,
-                sp= 30,
-                range.size=0.5,
-                sigma=0.1))
-  }
+    ## use a coalescent process to generate trees
+    source('simulation/src/coal/all/SimPhyloCoal.R')
+    source('simulation/src/coal/CoalMaster.R')
+    sim.dir <- 'simulation/saved/coal'
+    fig.dir <- 'figures/coal'
+    base.prms <- function() {
+        prms.f(list(ntree= 1, ## do not change
+                    nrule= 2,
+                    nmat= 1,
+                    nulls= 99,
+                    sp= 15,
+                    range.size=0.5,
+                    sigma=0.1))
+    }
 } else if(tree == 'bd'){
-  source('simulation/src/bd/SimPhyloBD.R')
-  source('simulation/src/bd/bdMaster.R')
-  sim.dir <- 'simulation/saved/bd'
-  fig.dir <- 'figures/bd'
-  base.prms <- function() {
-    prms.f(list(ntree= 1, ## do not change
-                nrule= 2,
-                nmat= 1,
-                nulls= 99,
-                sp= 30,
-                mu= 0.5,
-                lambda= 0.5,
-                age= 1000,
-                range.size=0.5,
-                sigma=0.1))
-  }
+    ## use a birth death process to generate trees
+    source('simulation/src/bd/SimPhyloBD.R')
+    source('simulation/src/bd/bdMaster.R')
+    sim.dir <- 'simulation/saved/bd'
+    fig.dir <- 'figures/bd'
+    base.prms <- function() {
+        prms.f(list(ntree= 1, ## do not change
+                    nrule= 2,
+                    nmat= 1,
+                    nulls= 99,
+                    sp= 15,
+                    mu= 0.5,
+                    lambda= 0.5,
+                    age= 1000,
+                    range.size=0.5,
+                    sigma=0.1))
+    }
 }
 
 
-source('figures/src/mod_by_nodf/mod_by_nodf_bar3.R')
-source('figures/src/mod_by_nodf/diff_bar3.R')
-source('figures/src/intimacy/intimacyDiff.R')
-source('figures/src/intimacy/intimacyDiff_image.R')
-source('figures/src/intimacy/intimacy2.R')
-source('figures/src/intimacy/intimacy3.R')
-source('figures/src/misc.R')
 
 
 prms.f <- function(prms) {
-  prms$combinations <- expand.grid(plants=1:prms$sp,
-                                   animals=1:prms$sp)
-  ## everything that is created once and used throughout should be in
-  ## here and then become a part of prms
-  prms
+    prms$combinations <- expand.grid(plants=1:prms$sp,
+                                     animals=1:prms$sp)
+    ## everything that is created once and used throughout should be in
+    ## here and then become a part of prms
+    prms
 }
 
 
+## plotting code. Not included here because most was not used in the
+## manuscript, can email me if you want it. lponisio@gmail.com
+
+## source('figures/src/mod_by_nodf/mod_by_nodf_bar3.R')
+## source('figures/src/mod_by_nodf/diff_bar3.R')
+## source('figures/src/intimacy/intimacyDiff.R')
+## source('figures/src/intimacy/intimacyDiff_image.R')
+## source('figures/src/intimacy/intimacy2.R')
+## source('figures/src/intimacy/intimacy3.R')
+## source('figures/src/misc.R')
